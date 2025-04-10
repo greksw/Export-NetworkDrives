800e55a6e62e114b57958b222b52b2e441295572
greksw <greksw@gmail.com>
2025-04-10 13:37:29 +0300

    Update Export-NetworkDrives.ps1
    Модификация скрипта для возможности экспорта в случае если подключение сетевых дисков выполняется по IP-адресу (например, \\192.168.1.100\share)

--- Export-NetworkDrives.ps1
+++ Export-NetworkDrives.ps1
@@ -1,42 +1,34 @@
-# Экспорт ВСЕХ постоянных сетевых дисков (из реестра)
-$outputFile = "C:\Temp\AllUsersMappedDrives.csv"
+# Export-MappedDrives.ps1
+$outputFile = "C:\Temp\MappedDrives_IP.csv"
+$keyFile = "C:\Temp\DriveEncryption.key"
 
-# Создаем папку, если ее нет
-if (-not (Test-Path -Path (Split-Path -Path $outputFile -Parent))) {
-    New-Item -ItemType Directory -Path (Split-Path -Path $outputFile -Parent) | Out-Null
+# Создаем ключ шифрования (если его нет)
+if (-not (Test-Path $keyFile)) {
+    $key = New-Object Byte[] 32
+    [Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($key)
+    $key | Out-File $keyFile -Force
+} else {
+    $key = Get-Content $keyFile
 }
 
-# Получаем диски всех пользователей из реестра
-$allDrives = @()
+# Получаем все подключенные диски (включая IP-адреса)
+$drives = Get-SmbMapping | Where-Object { $_.RemotePath -match '^\\\\[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\\' }
 
-# Текущий пользователь
-$currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
-$allDrives += Get-ItemProperty "HKCU:\Network\*" | Select-Object @{n="User";e={$currentUser}}, 
-    @{n="DriveLetter";e={$_.PSChildName}}, 
-    @{n="RemotePath";e={$_.RemotePath}}
-
-# Все пользователи системы (требует админских прав)
-if ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    $users = Get-ChildItem "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList" | ForEach-Object {
-        (Get-ItemProperty $_.PSPath).ProfileImagePath
-    }
+$exportData = foreach ($drive in $drives) {
+    $cred = cmdkey /list:$($drive.RemotePath) 2>$null | Where-Object { $_ -match 'Пользователь:' }
     
-    foreach ($user in $users) {
-        $sid = (New-Object System.Security.Principal.NTAccount($user.Split('\')[-1])).Translate([System.Security.Principal.SecurityIdentifier]).Value
-        $regPath = "Registry::HKEY_USERS\$sid\Network"
-        if (Test-Path $regPath) {
-            $drives = Get-ItemProperty "$regPath\*" -ErrorAction SilentlyContinue
-            foreach ($drive in $drives) {
-                $allDrives += [PSCustomObject]@{
-                    User = $user
-                    DriveLetter = $drive.PSChildName
-                    RemotePath = $drive.RemotePath
-                }
-            }
+    if ($cred) {
+        $username = ($cred -split 'Пользователь:')[1].Trim()
+        $password = Read-Host "Введите пароль для $($drive.LocalPath) ($username)" -AsSecureString
+        
+        [PSCustomObject]@{
+            DriveLetter = $drive.LocalPath
+            RemotePath  = $drive.RemotePath
+            Username    = $username
+            Password    = ConvertFrom-SecureString -SecureString $password -Key $key
         }
     }
 }
 
-# Экспорт в CSV
-$allDrives | Export-Csv -Path $outputFile -NoTypeInformation -Encoding UTF8
-Write-Host "Экспорт завершен. Данные сохранены в $outputFile"
+$exportData | Export-Csv $outputFile -NoTypeInformation -Encoding UTF8
+Write-Host "Данные сохранены в $outputFile. Ключ шифрования: $keyFile"
