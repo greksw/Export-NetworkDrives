687dff7e5ef84447cfc94ea0a7307c42638de814
greksw <greksw@gmail.com>
2025-04-10 13:33:36 +0300

    Update Export-NetworkDrives.ps1
    Модификация скрипта для экспорта списка сетевых дисков всех пользователей. 
    
    Для работы с реестром всех пользователей скрипт должен запускаться от имени Администратора
    
    Полное подключение дисков других пользователей требует:
    
    Либо выполнения скрипта под каждой учетной записью
    
    Либо использования специальных методов (например, планировщика задач)
    
    Постоянные (persistent) диски хранятся в реестре по пути:
    
    HKCU:\Network\ (для текущего пользователя)
    
    HKEY_USERS\<SID>\Network\ (для других пользователей)

--- Export-NetworkDrives.ps1
+++ Export-NetworkDrives.ps1
@@ -1,15 +1,42 @@
-# Скрипт для экспорта списка подключенных сетевых дисков
-$outputFile = "C:\Temp\MappedDrives.csv"
+# Экспорт ВСЕХ постоянных сетевых дисков (из реестра)
+$outputFile = "C:\Temp\AllUsersMappedDrives.csv"
 
-# Создаем папку, если она не существует
+# Создаем папку, если ее нет
 if (-not (Test-Path -Path (Split-Path -Path $outputFile -Parent))) {
     New-Item -ItemType Directory -Path (Split-Path -Path $outputFile -Parent) | Out-Null
 }
 
-# Получаем все подключенные сетевые диски
-$mappedDrives = Get-WmiObject -Class Win32_MappedLogicalDisk | Select-Object DeviceID, ProviderName, SessionID
+# Получаем диски всех пользователей из реестра
+$allDrives = @()
 
-# Экспортируем в CSV файл
-$mappedDrives | Export-Csv -Path $outputFile -NoTypeInformation -Encoding UTF8
+# Текущий пользователь
+$currentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+$allDrives += Get-ItemProperty "HKCU:\Network\*" | Select-Object @{n="User";e={$currentUser}}, 
+    @{n="DriveLetter";e={$_.PSChildName}}, 
+    @{n="RemotePath";e={$_.RemotePath}}
 
-Write-Host "Список сетевых дисков экспортирован в $outputFile"
+# Все пользователи системы (требует админских прав)
+if ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    $users = Get-ChildItem "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList" | ForEach-Object {
+        (Get-ItemProperty $_.PSPath).ProfileImagePath
+    }
+    
+    foreach ($user in $users) {
+        $sid = (New-Object System.Security.Principal.NTAccount($user.Split('\')[-1])).Translate([System.Security.Principal.SecurityIdentifier]).Value
+        $regPath = "Registry::HKEY_USERS\$sid\Network"
+        if (Test-Path $regPath) {
+            $drives = Get-ItemProperty "$regPath\*" -ErrorAction SilentlyContinue
+            foreach ($drive in $drives) {
+                $allDrives += [PSCustomObject]@{
+                    User = $user
+                    DriveLetter = $drive.PSChildName
+                    RemotePath = $drive.RemotePath
+                }
+            }
+        }
+    }
+}
+
+# Экспорт в CSV
+$allDrives | Export-Csv -Path $outputFile -NoTypeInformation -Encoding UTF8
+Write-Host "Экспорт завершен. Данные сохранены в $outputFile"
