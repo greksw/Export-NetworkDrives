f580d5a2c87d614066a4b2bc67d73e11e7d4a104
greksw <greksw@gmail.com>
2025-04-10 13:43:25 +0300

    Update Export-NetworkDrives.ps1
    Работает без подключения к сессиям пользователей - использует данные из реестра
    
    Сохраняет все ключевые данные:
    
    Имя пользователя
    
    Буква диска (с двоеточием, например "Z:")
    
    Сетевой путь
    
    SID пользователя
    Путь к профилю
    
    Не требует ввода паролей - экспортирует только данные о подключениях
    
    Работает для всех пользователей, которые когда-либо подключали сетевые диски на этом сервере

--- Export-NetworkDrives.ps1
+++ Export-NetworkDrives.ps1
@@ -1,34 +1,42 @@
-# Export-MappedDrives.ps1
-$outputFile = "C:\Temp\MappedDrives_IP.csv"
-$keyFile = "C:\Temp\DriveEncryption.key"
+# Export-AllUserDrives.ps1
+$outputFile = "C:\Temp\AllUserMappedDrives.csv"
 
-# Создаем ключ шифрования (если его нет)
-if (-not (Test-Path $keyFile)) {
-    $key = New-Object Byte[] 32
-    [Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($key)
-    $key | Out-File $keyFile -Force
-} else {
-    $key = Get-Content $keyFile
+# Создаем папку, если её нет
+if (-not (Test-Path "C:\Temp")) {
+    New-Item -ItemType Directory -Path "C:\Temp" -Force | Out-Null
 }
 
-# Получаем все подключенные диски (включая IP-адреса)
-$drives = Get-SmbMapping | Where-Object { $_.RemotePath -match '^\\\\[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\\' }
+# Получаем список всех профилей пользователей
+$userProfiles = Get-ChildItem "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList"
 
-$exportData = foreach ($drive in $drives) {
-    $cred = cmdkey /list:$($drive.RemotePath) 2>$null | Where-Object { $_ -match 'Пользователь:' }
+$results = foreach ($profile in $userProfiles) {
+    $sid = $profile.PSChildName
+    $profilePath = (Get-ItemProperty $profile.PSPath).ProfileImagePath
+    $username = $profilePath.Split('\')[-1]
     
-    if ($cred) {
-        $username = ($cred -split 'Пользователь:')[1].Trim()
-        $password = Read-Host "Введите пароль для $($drive.LocalPath) ($username)" -AsSecureString
+    # Проверяем ветку реестра пользователя
+    $regPath = "Registry::HKEY_USERS\$sid\Network"
+    
+    if (Test-Path $regPath) {
+        $driveLetters = Get-ChildItem $regPath
         
-        [PSCustomObject]@{
-            DriveLetter = $drive.LocalPath
-            RemotePath  = $drive.RemotePath
-            Username    = $username
-            Password    = ConvertFrom-SecureString -SecureString $password -Key $key
+        foreach ($drive in $driveLetters) {
+            $driveLetter = $drive.PSChildName
+            $remotePath = (Get-ItemProperty $drive.PSPath).RemotePath
+            
+            [PSCustomObject]@{
+                UserName = $username
+                DriveLetter = "$driveLetter`:"  # Добавляем двоеточие
+                RemotePath = $remotePath
+                SID = $sid
+                ProfilePath = $profilePath
+            }
         }
     }
 }
 
-$exportData | Export-Csv $outputFile -NoTypeInformation -Encoding UTF8
-Write-Host "Данные сохранены в $outputFile. Ключ шифрования: $keyFile"
+# Экспортируем в CSV
+$results | Export-Csv -Path $outputFile -NoTypeInformation -Encoding UTF8
+
+Write-Host "Экспорт завершен. Результаты сохранены в $outputFile"
+Write-Host "Найдено сетевых дисков: $($results.Count)"
